3046d09a6d09e991d613227502aa67b4303e70c1
Vladimir Garvardt <vgarvardt@gmail.com>
2014-07-19 23:41:39 +0400

    global ui variables in config for template

--- controller/init.go
+++ controller/init.go
@@ -3,9 +3,15 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/flosch/pongo2"
+
+	"../cfg"
 )
 
 func pongo2Render(c *gin.Context, templatePath string, ctx pongo2.Context) {
+	ctx["title"] = cfg.String("ui.title")
+	ctx["heading"] = cfg.String("ui.heading")
+	ctx["intro"] = cfg.String("ui.intro")
+
 	tpl, _ := pongo2.FromFile(templatePath)
 	out, _ := tpl.Execute(ctx)
 
