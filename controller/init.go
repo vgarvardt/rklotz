a67e92bf28c1f77a15fdcd0be16b35ba6f5fbc39
Vladimir Garvardt <vgarvardt@gmail.com>
2014-07-21 02:04:29 +0400

    split template into logical parts

--- controller/init.go
+++ controller/init.go
@@ -9,9 +9,11 @@ import (
 )
 
 func pongo2Render(c *gin.Context, template string, ctx pongo2.Context) {
-	ui := []string{"lang", "title", "heading", "intro", "theme"}
+	ui := []string{"lang", "title", "heading", "intro", "theme", "author", "description"}
 	for i := 0; i < len(ui); i++ {
-		ctx[ui[i]] = cfg.String("ui." + ui[i])
+		if _, ok := ctx[ui[i]]; !ok {
+			ctx[ui[i]] = cfg.String("ui." + ui[i])
+		}
 	}
 
 	tpl, _ := pongo2.FromFile(fmt.Sprintf("./templates/%v/%s", ctx["theme"], template))
