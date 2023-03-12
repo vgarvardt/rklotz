414972b8f14bce97ddb6daa3f3d1bea9df28124e
Vladimir Garvardt <vgarvardt@gmail.com>
2023-03-12 20:08:56 +0100

    refactor: switch to sethvargo envconfig

--- pkg/server/renderer/config.go
+++ pkg/server/renderer/config.go
@@ -7,24 +7,24 @@ import (
 
 // UIConfig is the configuration for user interface
 type UIConfig struct {
-	Theme       string `envconfig:"UI_THEME" default:"foundation6"`
-	Author      string `envconfig:"UI_AUTHOR" default:"Vladimir Garvardt"`
-	Email       string `envconfig:"UI_EMAIL" default:"vgarvardt@gmail.com"`
-	Description string `envconfig:"UI_DESCRIPTION" default:"rKlotz - simple golang-driven blog engine"`
-	Language    string `envconfig:"UI_LANGUAGE" default:"en"`
-	Title       string `envconfig:"UI_TITLE" default:"rKlotz"`
-	Heading     string `envconfig:"UI_HEADING" default:"rKlotz"`
-	Intro       string `envconfig:"UI_INTRO" default:"simple golang-driven blog engine"`
+	Theme       string `env:"UI_THEME,default=foundation6"`
+	Author      string `env:"UI_AUTHOR,default=Vladimir Garvardt"`
+	Email       string `env:"UI_EMAIL,default=vgarvardt@gmail.com"`
+	Description string `env:"UI_DESCRIPTION,default=rKlotz - simple golang-driven blog engine"`
+	Language    string `env:"UI_LANGUAGE,default=en"`
+	Title       string `env:"UI_TITLE,default=rKlotz"`
+	Heading     string `env:"UI_HEADING,default=rKlotz"`
+	Intro       string `env:"UI_INTRO,default=simple golang-driven blog engine"`
 	// DateFormat is format for posts, see http://golang.org/pkg/time/#Time.Format
-	DateFormat string `envconfig:"UI_DATEFORMAT" default:"2 Jan 2006"`
-	AboutPath  string `envconfig:"UI_ABOUT_PATH" default:"/etc/rklotz/about.tpl"`
+	DateFormat string `env:"UI_DATEFORMAT,default=2 Jan 2006"`
+	AboutPath  string `env:"UI_ABOUT_PATH,default=/etc/rklotz/about.tpl"`
 }
 
 // RootURLConfig is the configuration for app root url
 type RootURLConfig struct {
-	Scheme string `envconfig:"ROOT_URL_SCHEME" default:"http"`
-	Host   string `envconfig:"ROOT_URL_HOST"`
-	Path   string `envconfig:"ROOT_URL_PATH" default:"/"`
+	Scheme string `env:"ROOT_URL_SCHEME,default=http"`
+	Host   string `env:"ROOT_URL_HOST"`
+	Path   string `env:"ROOT_URL_PATH,default=/"`
 }
 
 // URL returns the URL for currently configured root url
