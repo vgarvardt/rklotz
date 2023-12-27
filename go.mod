0898b68973f721983c5bb280aa311d7d23bd66d7
Vladimir Garvardt <vgarvardt@gmail.com>
2023-12-27 21:14:59 +0100

    refactor: use testify mocks instead of custom hardcoded

--- go.mod
+++ go.mod
@@ -23,6 +23,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	go.etcd.io/bbolt v1.3.4 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- pkg/loader/loader_file_test.go
+++ pkg/loader/loader_file_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/cappuccinotm/slogx"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/vgarvardt/rklotz/pkg/formatter"
@@ -15,53 +16,52 @@ import (
 )
 
 type mockStorage struct {
-	saveCallCount  int
-	saveCallParams []*model.Post
-	saveCallResult []error
+	mock.Mock
 }
 
 func (s *mockStorage) Save(post *model.Post) error {
-	currentCall := s.saveCallCount
-	s.saveCallCount++
-	s.saveCallParams = append(s.saveCallParams, post)
-
-	return s.saveCallResult[currentCall]
+	args := s.Called(post)
+	return args.Error(0)
 }
 
 func (s *mockStorage) Finalize() error {
-	return nil
+	args := s.Called()
+	return args.Error(0)
 }
 
-func (s *mockStorage) FindByPath(string) (*model.Post, error) {
-	return nil, nil
+func (s *mockStorage) FindByPath(path string) (*model.Post, error) {
+	args := s.Called(path)
+	arg0 := args.Get(0)
+	if arg0 == nil {
+		return nil, args.Error(1)
+	}
+
+	return arg0.(*model.Post), args.Error(1)
 }
 
-func (s *mockStorage) ListAll(int) ([]*model.Post, error) {
-	return nil, nil
+func (s *mockStorage) ListAll(page int) ([]*model.Post, error) {
+	args := s.Called(page)
+	return args.Get(0).([]*model.Post), args.Error(1)
 }
 
-func (s *mockStorage) ListTag(string, int) ([]*model.Post, error) {
-	return nil, nil
+func (s *mockStorage) ListTag(tag string, page int) ([]*model.Post, error) {
+	args := s.Called(tag, page)
+	return args.Get(0).([]*model.Post), args.Error(1)
 }
 
 func (s *mockStorage) Close() error {
-	return nil
+	args := s.Called()
+	return args.Error(0)
 }
 
 func (s *mockStorage) Meta() *model.Meta {
-	return &model.Meta{
-		Posts:   len(s.saveCallParams),
-		PerPage: 0,
-		Pages:   0,
-	}
+	args := s.Called()
+	return args.Get(0).(*model.Meta)
 }
 
-func (s *mockStorage) TagMeta(string) *model.Meta {
-	return &model.Meta{
-		Posts:   len(s.saveCallParams),
-		PerPage: 0,
-		Pages:   0,
-	}
+func (s *mockStorage) TagMeta(tag string) *model.Meta {
+	args := s.Called(tag)
+	return args.Get(0).(*model.Meta)
 }
 
 func TestFileLoader_Load(t *testing.T) {
@@ -71,7 +71,15 @@ func TestFileLoader_Load(t *testing.T) {
 	// .../pkg/loader/../../assets/posts
 	postsBasePath := filepath.Join(wd, "..", "..", "assets", "posts")
 
-	storage := &mockStorage{saveCallResult: []error{nil, nil, nil}}
+	storage := new(mockStorage)
+
+	var savedPosts []*model.Post
+	storage.On("Save", mock.MatchedBy(func(post *model.Post) bool {
+		savedPosts = append(savedPosts, post)
+		return true
+	})).Return(nil).Times(3)
+
+	storage.On("Finalize").Return(nil).Once()
 
 	logger := slog.New(slogx.TestHandler(t))
 	f := formatter.New()
@@ -82,8 +90,7 @@ func TestFileLoader_Load(t *testing.T) {
 	err = fileLoader.Load(storage)
 	require.NoError(t, err)
 
-	require.Equal(t, 3, storage.saveCallCount)
-	require.Equal(t, 3, len(storage.saveCallParams))
+	storage.AssertExpectations(t)
 
 	for _, p := range []struct {
 		path  string
@@ -100,10 +107,11 @@ func TestFileLoader_Load(t *testing.T) {
 	}} {
 		t.Run(p.path, func(t *testing.T) {
 			var found bool
-			for _, post := range storage.saveCallParams {
+			for _, post := range savedPosts {
 				if post.Path == p.path {
 					found = true
 					assert.Equal(t, p.title, post.Title)
+					break
 				}
 			}
 			assert.True(t, found)
